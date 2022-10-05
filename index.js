c3bad750b3ed433ea9f57313ce66d5ab90ef75a1
Gustavo <gustavoalex.145@gmail.com>
2022-10-05 15:05:03 -0300

    primeira versão do app, falta finalizar a função de remover item

---
+++ index.js
@@ -0,0 +1,46 @@
+let itens = [];
+
+document.querySelector('input[type=submit]')
+.addEventListener('click', ()=>{
+    let nomeProduto = document.querySelector('input[name=nome_produto]');
+    let precoProduto = document.querySelector('input[name=valor_produto]');
+
+    itens.push({
+        nome: nomeProduto.value,
+        valor: precoProduto.value
+    });
+
+    let listaProdutos = document.querySelector('.lista-produtos')
+    let soma = 0;
+    listaProdutos.innerHTML = "";
+    itens.map(function(val){
+        soma+=parseFloat(val.valor)
+        listaProdutos.innerHTML+=`
+        <div class="lista-produto-single">
+        <h3>`+val.nome+`</h3>
+        <h3 class="price-produto"><span>R$`+val.valor+`</span></h3>
+            </div>
+        `;
+    })
+
+    soma = soma.toFixed(2);
+    nomeProduto.value = "";
+    precoProduto.value = "";
+
+    let elementoSoma = document.querySelector('.soma-produto h1')
+    elementoSoma.innerHTML = 'R$'+soma    
+
+})
+
+document.querySelector('button[name=limpar]')
+.addEventListener('click', ()=>{
+    itens = []
+    document.querySelector('.lista-produtos').innerHTML = "";
+    document.querySelector('.soma-produto h1').innerHTML = "R$ 0";
+})
+
+document.querySelector('button[name=remover-ultimo]')
+.addEventListener('click', ()=>{
+    itens.pop()
+    console.log(itens)
+})
